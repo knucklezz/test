61fa94550fbb37aa4419494c58eb5497a0690f02
Niklas Lundström <luni14@nti.umea>
2016-09-02 10:52:20 +0200

    Initial commit

---
+++ src/talOmvändare.java
@@ -0,0 +1,11 @@
+/**
+ * Created by luni14 on 2016-09-02.
+ */
+public class talOmvändare{
+
+public static void main(String[]args){
+        String[] numberList = {"1", "2", "3", "4"};
+
+        System.out.println(numberList);
+    }
+}
