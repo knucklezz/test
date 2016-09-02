a7c439611e40787981cf10f68411e8a4c5d12e19
luni14 <te14.niklas.lundstrom@umea.nti.se>
2016-09-02 11:26:47 +0200

    1

--- src/talOmvändare.java
+++ src/talOmvändare.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by luni14 on 2016-09-02.
  */
 public class talOmvändare{
 
-public static void main(String[]args){
-        String[] numberList = {"1", "2", "3", "4"};
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt;
 
-        System.out.println(numberList);
+
     }
 }
